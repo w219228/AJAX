05f704c40de288efc6d91d6867f62eca017d0ab7
willy <w219228@gmail.com>
2023-01-03 16:34:19 +0800

    加入專案檔案。

---
+++ AJAX/Controllers/AjaxController.cs
@@ -0,0 +1,38 @@
+﻿using AJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJAX.Controllers
+{
+	public class AjaxController : Controller
+	{
+		NorthwindContext _context =null;
+		public AjaxController(NorthwindContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public string Greet(string Name)
+		{
+			Thread.Sleep(3000);
+			return $"Hello:{Name}";
+		}
+		[HttpPost,ActionName("Greet")]
+		public string PostGreet(string Name) 
+		{
+			Thread.Sleep(3000);
+			return $"Hello:{Name}";
+		}
+		[HttpPost]
+		public string CheckName(string CompanyName) 
+		{
+			bool Exists=_context.Customers.Any(c=>c.CompanyName== CompanyName);
+			return Exists ? "true" : "false";
+		}
+
+		public async Task<JsonResult>Test(string Id)
+		{
+			return Json(_context.Customers.FindAsync(Id));
+		}
+	}
+}
---
+++ AJAX/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AJAX.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult CallGreet()
+		{
+			return View();
+		}
+
+		public IActionResult jQueryCallGreet()
+		{
+			return View();
+		}
+
+		public IActionResult CallCheckName()
+		{
+			return View();
+		}
+		public IActionResult jQueryCallCheckName()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
